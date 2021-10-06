88506e4916269339fe3560e0645d95c82ff398d0
violistt <66058945+violistt@users.noreply.github.com>
2021-10-06 17:53:39 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter
+
+canvas = tkinter.Canvas()
+canvas.pack()
+canvas.create_line(15, 10, 150, 220, width = 40, fill = 'blue')
+canvas.create_line(15, 10, 150, 220, width = 32, fill = 'yellow')
+canvas.create_line(15, 10, 150, 220, width = 25, fill = 'green')
+canvas.create_line(15, 10, 150, 220, width = 16, fill = 'blue')
+canvas.create_line(15, 10, 150, 220, width = 12, fill = 'black')
+canvas.create_line(15, 10, 150, 220, width = 9, fill = 'blue')
+canvas.create_line(15, 10, 150, 220, width = 5, fill = 'white')
+canvas.create_line(15, 10, 150, 220, width = 3, fill = 'black')
