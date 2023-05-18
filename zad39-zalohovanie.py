105abcd5945ee8267b078290a0bc9a10e83c13f2
violistt <66058945+violistt@users.noreply.github.com>
2023-05-18 13:41:33 +0200

    zad39-zalohovanie.py

--- zad39-zalohovanie.py
+++ zad39-zalohovanie.py
@@ -13,6 +13,7 @@ canvas.create_text(200, 100, text = anketa[0])
 odp = ["Áno", "Nie", "Neviem"]
 ludi = ano + nie + neviem
 farby = ['green', 'red', 'blue']
+hlasoval = 0
 for i in range(3):
     t = str(i + 1) + ') ' + odp[i] + ' - ' + anketa[1][i]
     canvas.create_text(200, i * 50 + 200, text = t, tags = 'graf')
@@ -20,37 +21,67 @@ for i in range(3):
     canvas.create_rectangle(270, i * 50 + 185, 270 + p, i * 50 + 215, fill = farby[i], outline = farby[i], tags = 'graf')
 
 def hlasuj_ano(event):
-    global ludi, ano
-    canvas.delete('graf')
-    ludi += 1
-    anketa[1][0] = str(int(anketa[1][0]) + 1)
-    for i in range(3):
-        t = str(i + 1) + ') ' + odp[i] + ' - ' + anketa[1][i]
-        canvas.create_text(200, i * 50 + 200, text = t)
-        p = (int(anketa[1][i]) / ludi) * 400
-        canvas.create_rectangle(270, i * 50 + 185, 270 + p, i * 50 + 215, fill = farby[i], outline = farby[i])
+    global hlasoval
+    if (not hlasoval):
+        global ludi, ano
+        canvas.delete('graf')
+        ludi += 1
+        anketa[1][0] = str(int(anketa[1][0]) + 1)
+        _anketa = open("anketa.txt", 'w')
+        for i in anketa:
+            for k, j in enumerate(i):
+                _anketa.write(j)
+                if (k != len(i) - 1):
+                    _anketa.write(' ')
+            _anketa.write('\n')
+        for i in range(3):
+            t = str(i + 1) + ') ' + odp[i] + ' - ' + anketa[1][i]
+            canvas.create_text(200, i * 50 + 200, text = t)
+            p = (int(anketa[1][i]) / ludi) * 400
+            canvas.create_rectangle(270, i * 50 + 185, 270 + p, i * 50 + 215, fill = farby[i], outline = farby[i])
+            hlasoval = 1
 
 def hlasuj_nie(event):
-    global ludi, ano
-    canvas.delete('graf')
-    ludi += 1
-    anketa[1][1] = str(int(anketa[1][1]) + 1)
-    for i in range(3):
-        t = str(i + 1) + ') ' + odp[i] + ' - ' + anketa[1][i]
-        canvas.create_text(200, i * 50 + 200, text = t)
-        p = (int(anketa[1][i]) / ludi) * 400
-        canvas.create_rectangle(270, i * 50 + 185, 270 + p, i * 50 + 215, fill = farby[i], outline = farby[i])
+    global hlasoval
+    if (not hlasoval):
+        global ludi, ano
+        canvas.delete('graf')
+        ludi += 1
+        anketa[1][1] = str(int(anketa[1][1]) + 1)
+        _anketa = open("anketa.txt", 'w')
+        for i in anketa:
+            for k, j in enumerate(i):
+                _anketa.write(j)
+                if (k != len(i) - 1):
+                    _anketa.write(' ')
+            _anketa.write('\n')
+        for i in range(3):
+            t = str(i + 1) + ') ' + odp[i] + ' - ' + anketa[1][i]
+            canvas.create_text(200, i * 50 + 200, text = t)
+            p = (int(anketa[1][i]) / ludi) * 400
+            canvas.create_rectangle(270, i * 50 + 185, 270 + p, i * 50 + 215, fill = farby[i], outline = farby[i])
+            hlasoval = 1
 
 def hlasuj_neviem(event):
-    global ludi, ano
-    canvas.delete('graf')
-    ludi += 1
-    anketa[1][2] = str(int(anketa[1][2]) + 1)
-    for i in range(3):
-        t = str(i + 1) + ') ' + odp[i] + ' - ' + anketa[1][i]
-        canvas.create_text(200, i * 50 + 200, text = t)
-        p = (int(anketa[1][i]) / ludi) * 400
-        canvas.create_rectangle(270, i * 50 + 185, 270 + p, i * 50 + 215, fill = farby[i], outline = farby[i])
+    global hlasoval
+    if (not hlasoval):
+        global ludi, ano
+        canvas.delete('graf')
+        ludi += 1
+        anketa[1][2] = str(int(anketa[1][2]) + 1)
+        _anketa = open("anketa.txt", 'w')
+        for i in anketa:
+            for k, j in enumerate(i):
+                _anketa.write(j)
+                if (k != len(i) - 1):
+                    _anketa.write(' ')
+            _anketa.write('\n')
+        for i in range(3):
+            t = str(i + 1) + ') ' + odp[i] + ' - ' + anketa[1][i]
+            canvas.create_text(200, i * 50 + 200, text = t)
+            p = (int(anketa[1][i]) / ludi) * 400
+            canvas.create_rectangle(270, i * 50 + 185, 270 + p, i * 50 + 215, fill = farby[i], outline = farby[i])
+        hlasoval = 1
 
 canvas.bind_all('1', hlasuj_ano)
 canvas.bind_all('2', hlasuj_nie)
